14df99e914d288d1b3b362df020ea06b21f1f3b5
Dmitriy <fr80@list.ru>
2019-03-05 22:22:56 +0300

    feat: add function for calculate zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    let tempBase = base;
+    let tempNumber = number;
+    let obj = {};
+    let result = 0;
+    let dividersSumm = 0;
+    for (let i = 2; i <= tempBase; i++) {
+        dividersSumm = 0;
+        tempNumber = number;
+        if (tempBase % i === 0) {
+            while (tempNumber > i) {
+                dividersSumm += Math.floor(tempNumber /= i);
+            }
+            obj[dividersSumm] = obj[dividersSumm] ? obj[dividersSumm] = obj[dividersSumm] + 1 : 1;
+            tempBase = tempBase / i;
+            i--;
+        }
+    }
+    for (let key in obj) {
+        result = result ? Math.min(result, Math.floor(key / obj[key])) : Math.floor(key / obj[key]);
+    }
+    return result;
+
+};
